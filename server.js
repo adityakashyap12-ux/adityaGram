f0070bbb6797c1a1b0c6dea9144fbcd505fc0551
Aditya Kashyap <aditya@adityaGram.com>
2026-09-13 23:48:08 +0530

    backend live fix

--- server.js
+++ server.js
@@ -5,34 +5,31 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname)); // This will serve login.html and signup.html
+app.use(express.static(__dirname));
 
-let users = []; // temporary memory
+let users = [];
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
 });
 
-app.post('/signup', (req, res) => {
+app.post('/signup', (req,res) => {
   const { email, password } = req.body;
-  const exists = users.find(u => u.email === email);
-  if (exists) {
-    return res.json({ success: false, message: 'User already exists!' });
+  if(users.find(u => u.email == email)){
+    return res.json({ success: false, message: "User already exists!" });
   }
   users.push({ email, password });
-  console.log('New user:', email);
-  res.json({ success: true, message: 'Signup Successful! Go to Login.' });
+  console.log("New user:", email);
+  res.json({ success: true, message: "Signup Successful! Go to Login.." });
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', (req,res) => {
   const { email, password } = req.body;
-  const user = users.find(u => u.email === email && u.password === password);
-  if (user) {
-    res.json({ success: true, message: 'Login Successful!' });
-  } else {
-    res.json({ success: false, message: 'Wrong email or password!' });
-  }
+  const user = users.find(u => u.email == email && u.password == password);
+  if(user) res.json({ success: true, message: "Login Successful!" });
+  else res.json({ success: false, message: "Wrong email or password!" });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT|| 10000, () => {
+  console.log("AdityaGram running on http://localhost:3000");
+});
