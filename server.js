52194aa3d48fe71994da5faca654ccb099be64d5
Aditya Kashyap <aditya@adityaGram.com>
2026-09-14 00:22:15 +0530

    fix AdityaGram folder routes

--- server.js
+++ server.js
@@ -1,43 +1,26 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// Serve all files
 app.use(express.static(__dirname));
-
-let users = [];
+app.use('/AdityaGram', express.static(path.join(__dirname, 'AdityaGram')));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+  res.sendFile(path.join(__dirname, "AdityaGram", "index.html"));
 });
 
 app.get('/signup.html', (req,res) => {
-  res.sendFile(path.join(__dirname, "signup.html"));
+  res.sendFile(path.join(__dirname, "AdityaGram", "signup.html"));
 });
 
-app.get('/index.html', (req,res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/AdityaGram/index.html', (req,res) => {
+  res.sendFile(path.join(__dirname, "AdityaGram", "index.html"));
 });
 
-app.post('/signup', (req,res) => {
-  const { email, password } = req.body;
-  if(users.find(u => u.email == email)){
-    return res.json({ success: false, message: "User already exists!" });
-  }
-  users.push({ email, password });
-  console.log("New user:", email);
-  res.json({ success: true, message: "Signup Successful! Go to Login.." });
+app.get('/AdityaGram/signup.html', (req,res) => {
+  res.sendFile(path.join(__dirname, "AdityaGram", "signup.html"));
 });
 
-app.post('/login', (req,res) => {
-  const { email, password } = req.body;
-  const user = users.find(u => u.email == email && u.password == password);
-  if(user) res.json({ success: true, message: "Login Successful!" });
-  else res.json({ success: false, message: "Wrong email or password!" });
-});
-
-app.listen(process.env.PORT|| 10000, () => {
-  console.log("AdityaGram running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('running'));
