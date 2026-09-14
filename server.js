bef4b9ba19c02f1a23a3a4155d5624b455d6423c
Aditya Kashyap <aditya@adityaGram.com>
2026-09-14 11:37:05 +0530

    fix server.js static routes

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve all files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// dono jagah se files serve karega - root se bhi aur AdityaGram se bhi
+app.use(express.static(path.join(__dirname, 'AdityaGram')));
 app.use(express.static(__dirname));
-app.use('/AdityaGram', express.static(path.join(__dirname, 'AdityaGram')));
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, "AdityaGram", "index.html"));
+// login/signup ke liye direct route
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'AdityaGram', 'login.html'));
 });
 
-app.get('/signup.html', (req,res) => {
-  res.sendFile(path.join(__dirname, "AdityaGram", "signup.html"));
+app.get('/signup.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'AdityaGram', 'signup.html'));
 });
 
-app.get('/AdityaGram/index.html', (req,res) => {
-  res.sendFile(path.join(__dirname, "AdityaGram", "index.html"));
-});
-
-app.get('/AdityaGram/login.html', (req,res) => {
-  res.sendFile(path.join(__dirname, "AdityaGram", "login.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'AdityaGram', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('running'));
+app.listen(PORT, () => console.log('Server running'));
